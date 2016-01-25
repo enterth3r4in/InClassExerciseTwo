a9cd7c20b012e7dffa30e0fc8b6e15fefbd888c2
enterth3r4in <believe.the.rain@gmail.com>
2016-01-25 15:25:26 -0500

    In Class Exercise
    Bad Joke Generator

---
+++ InClassExerciseTwo/InClassExerciseTwo/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace InClassExerciseTwo
+{
+    partial class formJokeGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formJokeGenerator));
+            this.btnSetup = new System.Windows.Forms.Button();
+            this.btnPunchline = new System.Windows.Forms.Button();
+            this.lblJoke = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSetup
+            // 
+            this.btnSetup.Location = new System.Drawing.Point(12, 12);
+            this.btnSetup.Name = "btnSetup";
+            this.btnSetup.Size = new System.Drawing.Size(179, 39);
+            this.btnSetup.TabIndex = 0;
+            this.btnSetup.Text = "Joke";
+            this.btnSetup.UseVisualStyleBackColor = true;
+            this.btnSetup.Click += new System.EventHandler(this.btnSetup_Click);
+            // 
+            // btnPunchline
+            // 
+            this.btnPunchline.Location = new System.Drawing.Point(671, 12);
+            this.btnPunchline.Name = "btnPunchline";
+            this.btnPunchline.Size = new System.Drawing.Size(191, 39);
+            this.btnPunchline.TabIndex = 1;
+            this.btnPunchline.Text = "Punchline";
+            this.btnPunchline.UseVisualStyleBackColor = true;
+            this.btnPunchline.Click += new System.EventHandler(this.btnPunchline_Click);
+            // 
+            // lblJoke
+            // 
+            this.lblJoke.AutoSize = true;
+            this.lblJoke.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblJoke.Font = new System.Drawing.Font("Jokerman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJoke.Location = new System.Drawing.Point(12, 219);
+            this.lblJoke.Name = "lblJoke";
+            this.lblJoke.Size = new System.Drawing.Size(2, 26);
+            this.lblJoke.TabIndex = 2;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(375, 322);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(119, 34);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "ABORT!";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::InClassExerciseTwo.Properties.Resources.Laugh;
+            this.pictureBox1.Location = new System.Drawing.Point(368, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 127);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // formJokeGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 368);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblJoke);
+            this.Controls.Add(this.btnPunchline);
+            this.Controls.Add(this.btnSetup);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "formJokeGenerator";
+            this.Text = "Bad Jokes";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSetup;
+        private System.Windows.Forms.Button btnPunchline;
+        private System.Windows.Forms.Label lblJoke;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ InClassExerciseTwo/InClassExerciseTwo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InClassExerciseTwo
+{
+    public partial class formJokeGenerator : Form
+    {
+        string joke1 = "How many programmers does it take to screw in a lightbulb?";
+        string joke2 = "Why do programmers always mix up Halloween and Christmas?";
+        string joke3 = "What's the difference between an enterprise software salesperson and a used car dealer?";
+
+        string punchline1 = "None. That's a hardware problem.";
+        string punchline2 = "Because Oct 31 == Dec 25";
+        string punchline3 = "The used car dealer knows when he's lying.";
+
+        int jokeCounter = 0;
+
+        public formJokeGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSetup_Click(object sender, EventArgs e)
+        {
+            lblJoke.Text = string.Empty;
+            if(jokeCounter == 3)
+            {
+                jokeCounter = 0;
+            }
+            switch(jokeCounter + 1)
+            {
+                case 1:
+                    lblJoke.Text = joke1;
+                    break;
+                case 2:
+                    lblJoke.Text = joke2;
+                    break;
+                case 3:
+                    lblJoke.Text = joke3;
+                    break;
+            }
+            jokeCounter++;
+        }
+
+        private void btnPunchline_Click(object sender, EventArgs e)
+        {
+            switch (jokeCounter)
+            {
+                case 1:
+                    MessageBox.Show(punchline1);
+                    break;
+                case 2:
+                    MessageBox.Show(punchline2);
+                    break;
+                case 3:
+                    MessageBox.Show(punchline3);
+                    break;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You've made the right choice!");
+            this.Close();
+        }
+    }
+}
