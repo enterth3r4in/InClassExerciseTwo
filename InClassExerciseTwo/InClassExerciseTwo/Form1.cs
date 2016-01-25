bf2743cd900e349e487a7e1f81258a68b698d2a6
enterth3r4in <believe.the.rain@gmail.com>
2016-01-25 15:36:52 -0500

    Allowed easier expansion of listed jokes

--- InClassExerciseTwo/InClassExerciseTwo/Form1.cs
+++ InClassExerciseTwo/InClassExerciseTwo/Form1.cs
@@ -15,7 +15,9 @@ using System.Windows.Forms;
  * Input:  This program does not accept inputs
  * Desired Output:  The label on the form should show the joke and a message box should pop up with the punchline
  * Variables and Classes: strings for all the jokes and punchlines because I don't know how to do arrays yet =(,
- *          joke counter to keep track of what order to do the jokes in
+ *          joke counter to keep track of what order to do the jokes in,
+ *          maxJokes allows easier expansion of adding more joke. Simply add the strings for the joke and punchline
+ *          and increment the number by one.
  * Formulas: N/A
  * Description of the main algorithm: The label is filled with the text based on a switch statement and the
  *          message box is filled based on the same counter. The counter, when reaching the end of the list of jokes
@@ -31,29 +33,33 @@ namespace InClassExerciseTwo
 {
     public partial class formJokeGenerator : Form
     {
+        //Jokes are held in strings to allow easier expansion
         string joke1 = "How many programmers does it take to screw in a lightbulb?";
         string joke2 = "Why do programmers always mix up Halloween and Christmas?";
         string joke3 = "What's the difference between an enterprise software salesperson and a used car dealer?";
 
+        //Punchlines also held in strings to allow easier expansion
         string punchline1 = "None. That's a hardware problem.";
         string punchline2 = "Because Oct 31 == Dec 25";
         string punchline3 = "The used car dealer knows when he's lying.";
 
-        int jokeCounter = 0;
+        int jokeCounter = 0; //Joke Counter to keep track of current joke number
+        int maxJokes = 3; //Max number of jokes in the list to allow easy expansion
 
         public formJokeGenerator()
         {
             InitializeComponent();
         }
 
+        //Sets up the joke based on jokeCounter (modified + 1)
         private void btnSetup_Click(object sender, EventArgs e)
         {
-            lblJoke.Text = string.Empty;
-            if(jokeCounter == 3)
+            lblJoke.Text = string.Empty; //Defaults the label to empty for safety
+            if(jokeCounter == maxJokes)
             {
                 jokeCounter = 0;
             }
-            switch(jokeCounter + 1)
+            switch(jokeCounter + 1) //Probably ineffecient to add 1 and switch that. Might be better to change the cases.
             {
                 case 1:
                     lblJoke.Text = joke1;
@@ -67,7 +73,8 @@ namespace InClassExerciseTwo
             }
             jokeCounter++;
         }
-
+        
+        //Simple enough, uses a switch on the unmodified jokeCounter number.
         private void btnPunchline_Click(object sender, EventArgs e)
         {
             switch (jokeCounter)
@@ -84,6 +91,7 @@ namespace InClassExerciseTwo
             }
         }
 
+        //Exits the program after displaying another HILARIOUS joke.
         private void btnExit_Click(object sender, EventArgs e)
         {
             MessageBox.Show("You've made the right choice!");
